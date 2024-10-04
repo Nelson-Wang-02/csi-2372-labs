f7434014c4407a8487ba9546a4398920d5243261
Nelson Wang <nelsonwangmain@outlook.com>
2024-10-03 22:48:33 -0400

    Complete Exercise 1

---
+++ lab2/myFile1.cpp
@@ -0,0 +1,35 @@
+/*code*/
+#include <iostream>
+using namespace std;
+int main(void) {
+	int tab[10];
+	int* p;
+	for (int i = 0; i < 10; i++) {
+		tab[i] = i * i;
+	}
+
+	cout << tab[2] << endl;
+	cout << tab[1] << endl;
+
+	tab[2] = tab[1];
+
+	cout << tab[2] << endl;
+
+	tab[2] = *(tab + 1);
+
+	*(tab + 2) = tab[1];
+
+	cout << *(tab + 2) << endl;
+
+	*(tab + 2) = *(tab + 1);
+
+	cout << *(tab + 2) << endl;
+
+	p = &tab[0];
+	p = tab + 1;
+
+	tab[4] = *p;
+
+	cout << tab[4] << endl;
+
+}
