91d1d1da42657a06787181f1b8ae7193b97bc2af
Nelson Wang <nelsonwangmain@outlook.com>
2024-10-06 23:08:26 -0400

    Complete Exercise 2

---
+++ lab2/myFile2.cpp
@@ -0,0 +1,40 @@
+/*myFile2.cpp : Ex2 a2 CSI2372A*/
+
+#include "myFile2.h"
+
+int main() {
+	int myArray[sizeArray] = { 2,4,88,20,3,55,87,134,2,5 };
+
+	cout << "Displaying the unsorted array :" << endl;
+	for (int i = 0; i < sizeArray; i++) {
+		cout << myArray[i] << " ";
+	}
+	cout <<  endl << endl;
+	sort(myArray, sizeArray);
+	cout << "Displaying the sorted array :" << endl;
+	for (int i = 0; i < sizeArray; i++) {
+		cout << myArray[i] << " ";
+	}
+	cout << endl;
+}
+
+void sort(int a[], int size)
+{
+	//YOUR CODE HERE
+	for (int i = 1; i < size; i++) {
+		int key = a[i]; 
+		int j = i - 1;
+		
+		// Loop backwards until j < 0 and key > element.
+		while (j >= 0 && key < a[j]) {
+			
+			a[j + 1] = a[j];
+			
+			j--;
+		}
+
+		a[j + 1] = key;
+		
+	}
+
+}
