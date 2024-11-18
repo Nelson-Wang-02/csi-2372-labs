d5502baa49165f3a4decdc3927805f257b2f756b
Nelson Wang <nelsonwangmain@outlook.com>
2024-11-18 15:54:39 -0500

    Create required files.

---
+++ project/Card.cpp
@@ -0,0 +1,46 @@
+#include "include.h"
+
+class Card {
+    private:
+        /* data */
+
+
+    public:
+        virtual int getCardsPerCoin(int coins) = 0;
+        virtual string getName() = 0;
+        virtual void print(ostream& out) = 0;
+};
+
+
+class Blue {
+
+};
+
+class Chili {
+
+};
+
+class Stink {
+
+};
+
+class Green {
+
+};
+
+class Soy {
+
+};
+
+class Black {
+
+};
+
+class Red {
+
+};
+
+class Garden {
+
+};
+
